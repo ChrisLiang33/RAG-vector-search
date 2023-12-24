85876256e3135ea74ea87e0f5f94fadbacb75dc7
Chrisliang33 <liangxianmai@yahoo.com>
2023-12-23 22:19:40 -0500

    update

---
+++ key_param.py
@@ -0,0 +1,2 @@
+openai_api_key = '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'
+MONGO_URI = 'mongodb+srv://xianmailiang:<Chrisliang8!>@cluster0.poavnb9.mongodb.net/?retryWrites=true&w=majority'
